cbaf407566f6a6d3a5ff42d512d27165b08ab004
Arakku <100070895+Arakku@users.noreply.github.com>
2022-03-29 01:40:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  <ul>
  <li><a href="index.php">Home</a></li>
  <li><a href="LT2/THE RECIPE WEBSITES/Recipe Website.html"> LEARNING TASK 2 </a></li>
- <li><a href="LT3/Top 10 Celebrities.html"> LEARNING TASK 3 </a></li>
+ <li><a href="LT3/10 FAVORITE CELEBRITIRES WEBSITE/Top 10 Celebrities.html"> LEARNING TASK 3 </a></li>
  <li> <a href="CLE1MT/CASE STUDY WEBSITE (POVERTY)/index.html"> MIDTERM</a></li>
   
  </ul>
