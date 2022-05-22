a35b00fa6f5ed57fc81b1cb4bd30ce59bf24eba7
Arakku <100070895+Arakku@users.noreply.github.com>
2022-05-23 01:47:53 +0800

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,188 @@
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Portfolio Website</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1" />
+
+  <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&family=Russo+One&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" type="text/css" href="CSS/homepage.css">
+</head>
+<body>
+
+  <section class="s1">
+    <div class="main-container">
+      <div class="greeting-part">
+        <h1>Welcome to my Portfolio!</h1>
+      </div>
+
+      <div class="intro-part">
+        <div class="nav-part">
+          
+
+          <ul id="navigation">
+            <center><p><a><li><b><h5>HOME</h5></p></a></li></b></center>
+
+          </ul>
+        </div>
+
+        <div class="left-side">
+          <img id="profile_pic" src="Pics/Developer.jpg">
+
+        </div>
+
+        <div class="right-side">
+              <h3>About Me</h3>
+              <p>Hi Guys! I am Gideon P. Calderon an Web Developer and Designer. Who's Making Websites for fun.</p>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+  <section class="s2">
+    <div class="main-container">
+      <div class="about-section">
+        <div class="about-me">
+
+           <ul id="navigation">
+            <center><b><li><p><h5>PROFILE</h5></p></a></li></center></b>
+          </ul>
+
+
+          <h4>MY ACHIEVEMENTS</h4>
+
+
+          <p><h5><center>EDUCATIONAL ATTAINMENT</center></h5></b></p>
+
+          <p>1.)MCC - <b> 2015-2019</b></p><br>
+        </br>
+
+        <p>2.)STI WNU -<b> 2019-2021</b></p><br>
+        </br>
+
+        <p>3.)USLS - <b> 2021-2026</b></p><br>
+        </br>
+
+
+
+
+          <p><h5><center>WORK CAREERS</center></h5></b></p>
+
+          <p>1.)Web Developer at Google - <b>2016-2018</b></p><br>
+        </br>
+
+        <p>2.)Web Designer at Fisher Corp. - <b>2018-2020</b></p><br>
+        </br>
+
+        <p>3.)Web Analyst at Panasonic - <b>2020-current</b></p><br>
+        </br>
+
+
+          <hr>
+
+
+          <h4>EXPERTISE</h4>
+
+          <p>Web Developer & Designer with professional use React:</p>
+
+          <div id="skill-set">
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>Sublime</li>
+              <li>Visual Studio</li>
+            </ul>
+
+            <ul>
+              <li>AWS</li>
+              <li>Heroku</li>
+            </ul>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+
+
+
+
+
+  <section class="s3">
+    <ul id="navigation">
+            <li><p><b><h5>PROJECTS<h5></p></a></li></b>
+
+          </ul>
+    <div class="main-container">
+      <h3 style="text-align: center;">Previous Works</h3>
+
+
+      <div class="post-section">
+
+        <div>
+          <div class="post">
+            <img class="thumbnail" src="Pics/Recipe.jpeg">
+            <div class="post-preview">
+              <h6 class="post-title">Recipe Website</h6>
+              <p class="post-intro">The List of Ingredients and Procedure when cooking the recipe.</p>
+              <a href="post.html">Click To View</a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div class="post">
+            <img class="thumbnail" src="Pics/Celebrity.jpeg">
+            <div class="post-preview">
+              <h6 class="post-title">Celebrity Website</h6>
+              <p class="post-intro">An List of Your Top 10 Celebrities that you admire.</p>
+              <a href="post.html">Click To View</a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div class="post">
+            <img class="thumbnail" src="Pics/Poverty.jpeg">
+            <div class="post-preview">
+              <h6 class="post-title">Poverty Website</h6>
+              <p class="post-intro">An Documentary Site which tells the study and calculations of people who are affected by Poverty.</p>
+              <a href="CASE STUDY WEBSITE (POVERTY)/Poverty.html">Click To View</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+
+  
+</body>
+</html>
